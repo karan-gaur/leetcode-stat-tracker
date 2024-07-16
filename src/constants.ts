cf934ff63b85128089803d62449a904e7dd2df50
Karan Gaur <karan.gopal.gaur@gmail.com>
2024-07-16 15:42:44 -0400

    Adding base code

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+import { ThemeInput } from "react-activity-calendar";
+
+export const LEETCODE_API_URL = "https://leetcode-stats-api.herokuapp.com";
+
+export const DEFAULT_THEME: ThemeInput = {
+    light: ['#f5f5f5', '#fc835d', '#d172db'],
+    dark: ['#242323', '#e04616', '#7c1687'],
+  };
---
+++ src/type.ts
@@ -0,0 +1,59 @@
+export interface SubmissionData {
+    date: string;
+    level: number;
+}
+
+export interface LeetcodeProps {
+    username: string;
+    labels?: Record<string, string>;
+    transformTotalCount?: boolean;
+    errorMessage?: string;
+    [key: string]: any;
+}
+
+export interface SubmissionCalendar {
+    [timestamp: string]: number;
+}
+ 
+export interface ApiErrorResponse {
+    error: string;
+}
+
+export interface LeetcodeData {
+    status: string;
+    message: string;
+    totalSolved: number;
+    totalQuestions: number;
+    easySolved: number;
+    totalEasy: number;
+    mediumSolved: number;
+    totalMedium: number;
+    hardSolved: number;
+    totalHard: number;
+    acceptanceRate: number;
+    ranking: number;
+    contributionPoints: number;
+    reputation: number;
+    submissionCalendar: SubmissionCalendar;
+}
+
+export interface Activity {
+    date: string;
+    count: number;
+    level: 0 | 1 | 2 | 3 | 4;
+}
+
+export type Year = number | 'last';
+
+type Color = string;
+type ColorScale = [Color, Color, Color, Color, Color];
+
+export type ThemeInput =
+  | {
+      light: ColorScale | [from: Color, to: Color];
+      dark?: ColorScale | [from: Color, to: Color];
+    }
+  | {
+      light?: ColorScale | [from: Color, to: Color];
+      dark: ColorScale | [from: Color, to: Color];
+    };
