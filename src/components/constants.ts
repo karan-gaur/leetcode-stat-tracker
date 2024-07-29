6af0df6b69d694a641d217df7976e88d1e25de52
Karan Gaur <karan.gopal.gaur@gmail.com>
2024-07-29 13:29:56 -0400

    Adding componenets

---
+++ src/components/constants.ts
@@ -0,0 +1,8 @@
+import { ThemeInput } from "react-activity-calendar";
+
+export const LEETCODE_API_URL = "https://leetcode-stats-api.herokuapp.com";
+
+export const DEFAULT_THEME: ThemeInput = {
+  light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
+  dark: ["#161b22", "#0e4429", "#006d32", "#26a641", "#39d353"],
+};
